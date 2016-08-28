521ce85edad4da019fd8c86546cc03eacf9b841f
Andreas <alanz@gmx.de>
2016-08-28 15:31:09 +0200

    Adding FileChangeListenerService to watch for modifications of the stt-file

---
+++ src/main/java/org/stt/event/FileChangeListenerService.java
@@ -0,0 +1,123 @@
+package org.stt.event;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.ClosedWatchServiceException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.stt.Service;
+import org.stt.model.FileChanged;
+import org.stt.persistence.stt.STTFile;
+
+import com.google.common.eventbus.EventBus;
+import com.google.inject.Inject;
+
+import javafx.application.Platform;
+
+public class FileChangeListenerService implements Service
+{
+	
+	Logger LOG = Logger.getLogger(FileChangeListenerService.class.getName());
+
+	private EventBus eventBus;
+	private WatchService watchService;
+
+	private Path sttFile;
+
+	static class WatchHandler extends Thread
+	{
+		Logger LOG = Logger.getLogger(WatchHandler.class.getName());
+		
+		private Path fileToWatch;
+		private EventBus eventBus;
+		private WatchService watchService;
+
+		public WatchHandler(Path fileToWatch, WatchService watchService, EventBus eventBus) {
+			this.fileToWatch = checkNotNull(fileToWatch);
+			this.watchService = checkNotNull(watchService);
+			this.eventBus = checkNotNull(eventBus);
+			
+
+		}
+		
+		@Override
+		public void run()
+		{
+
+			try {
+				Path parentDir = fileToWatch.getParent();
+				if (parentDir == null)
+					return;
+				parentDir.register(watchService, 
+						StandardWatchEventKinds.ENTRY_CREATE,
+			            StandardWatchEventKinds.ENTRY_DELETE, 
+			            StandardWatchEventKinds.ENTRY_MODIFY);
+				
+				while (!Thread.interrupted()) {
+					try {
+						WatchKey watchKey = watchService.take();
+						List<WatchEvent<?>> pollEvents = watchKey.pollEvents();
+						for (WatchEvent<?> event : pollEvents)
+						{
+							Path changedFile = (Path) event.context();
+							
+							if (fileToWatch.endsWith(changedFile))
+							{
+								Platform.runLater(new Runnable() {
+									
+									@Override
+									public void run() {
+										eventBus.post(new FileChanged(changedFile));
+									}
+								});
+								
+							}
+						}
+						watchKey.reset();
+					} catch (InterruptedException | ClosedWatchServiceException e) {
+						Thread.currentThread().interrupt();
+					}
+				}
+			} catch (Exception e) {
+				// Ignore
+				LOG.log(Level.SEVERE, "Exception in WatchHandler", e);
+			}
+		}
+	}
+	
+
+	@Inject
+	public FileChangeListenerService(@STTFile File file, EventBus eventBus) {
+		this.sttFile = checkNotNull(file).toPath();
+		this.eventBus = checkNotNull(eventBus);
+	}
+	
+	@Override
+	public void start() throws Exception {
+		watchService = FileSystems.getDefault().newWatchService();
+		
+		WatchHandler handler = new WatchHandler(sttFile, watchService, eventBus);
+		handler.start();
+	}
+
+	@Override
+	public void stop() {
+		try {
+			if (watchService != null)
+				watchService.close();
+		} catch (IOException e) {
+			LOG.log(Level.SEVERE, "Exception while closing watch service", e);
+		}
+		watchService = null;
+	}
+}
